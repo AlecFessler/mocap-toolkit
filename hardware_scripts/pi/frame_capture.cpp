cbe801077ba0685a8b15640b1ade53eff2741e88
Alec Fessler <afessler1998@gmail.com>
2024-08-15 16:18:34 -0500

    updated error codes returned to be more descriptive

--- hardware_scripts/pi/frame_capture.cpp
+++ hardware_scripts/pi/frame_capture.cpp
@@ -26,7 +26,12 @@ void sig_handler(int signo, siginfo_t *info, void *context) {
 
 int main() {
   PCtx& pctx = PCtx::getInstance();
-  pctx.logger = std::make_unique<Logger>("logs.txt");
+
+  try {
+    pctx.logger = std::make_unique<Logger>("logs.txt");
+  } catch (const std::exception& e) {
+    return -EIO;
+  }
 
   struct sigaction action;
   pid_t pid = getpid();
@@ -37,41 +42,46 @@ int main() {
       sigaction(SIGINT, &action, NULL) < 0 ||
       sigaction(SIGTERM, &action, NULL) < 0) {
     pctx.logger->log(Logger::timestamp(), Logger::Level::ERROR, __FILE__, __LINE__, "Failed to set signal handler");
-    return -1;
+    return -EINVAL;
   }
 
   cpu_set_t cpuset;
   CPU_ZERO(&cpuset);
   CPU_SET(3, &cpuset);
   if (sched_setaffinity(0, sizeof(cpuset), &cpuset) < 0) {
-    pctx.logger->log(Logger::timestamp(), Logger::Level::ERROR, __FILE__, __LINE__, "Failed to set CPU affinity");
-    return -1;
+    pctx.logger->log(Logger::timestamp(), Logger::Level::ERROR, __FILE__, __LINE__, "Failed to set CPU affinity, likely due to insufficient permissions");
+    return -EPERM;
   }
 
   struct sched_param param;
   param.sched_priority = sched_get_priority_max(SCHED_FIFO);
   if (sched_setscheduler(0, SCHED_FIFO, &param) < 0) {
-    pctx.logger->log(Logger::timestamp(), Logger::Level::ERROR, __FILE__, __LINE__, "Failed to set scheduler");
-    return -1;
+    pctx.logger->log(Logger::timestamp(), Logger::Level::ERROR, __FILE__, __LINE__, "Failed to set scheduler policy, likely due to insufficient permissions");
+    return -EPERM;
   }
 
   int fd;
   fd = open("/proc/gpio_interrupt_pid", O_WRONLY);
   if (fd < 0) {
     pctx.logger->log(Logger::timestamp(), Logger::Level::ERROR, __FILE__, __LINE__, "Failed to open /proc/gpio_interrupt_pid");
-    return -1;
+    return -errno;
   }
   if (dprintf(fd, "%d", pid) < 0) {
     pctx.logger->log(Logger::timestamp(), Logger::Level::ERROR, __FILE__, __LINE__, "Failed to write to /proc/gpio_interrupt_pid");
     close(fd);
-    return -1;
+    return -errno;
   }
   close(fd);
 
   std::pair<unsigned int, unsigned int> resolution = std::make_pair(1920, 1080);
   int buffersCount = 4;
   std::pair<std::int64_t, std::int64_t> frameDurationLimits = std::make_pair(16667, 16667);
-  pctx.cam = std::make_unique<CameraHandler>(resolution, buffersCount, frameDurationLimits);
+  try {
+    pctx.cam = std::make_unique<CameraHandler>(resolution, buffersCount, frameDurationLimits);
+  } catch (const std::exception& e) {
+    pctx.logger->log(Logger::timestamp(), Logger::Level::ERROR, __FILE__, __LINE__, "Failed to initialize camera");
+    return -EIO;
+  }
 
   pctx.running = 1;
   while (pctx.running) {
