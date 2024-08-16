c0d2eb2e398879d251a98b1d0f4896dc2482ca93
Alec Fessler <afessler1998@gmail.com>
2024-08-16 10:09:39 -0500

    repo missing arduino dir

--- hardware_scripts/pi/PCtx.cpp
+++
@@ -1,9 +0,0 @@
-#include "PCtx.h"
-
-PCtx::PCtx() noexcept
-  : running(0), queueRequest(0), requestComplete(0), logger(), cam() {}
-
-PCtx& PCtx::getInstance() noexcept {
-    static PCtx instance;
-    return instance;
-}
--- hardware_scripts/pi/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-g++ *.cpp -o vidcap -pthread -lrt `pkg-config --cflags --libs libcamera`
-
-sudo setcap cap_sys_nice+ep ./vidcap
