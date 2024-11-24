553102ee9316407085f1de79e8b331ffdecc663e
Alec Fessler <afessler1998@gmail.com>
2024-11-23 19:29:05 -0500

    added logging to video endcoder

--- dataset_gen/pi/src/videnc.cpp
+++ dataset_gen/pi/src/videnc.cpp
@@ -2,10 +2,13 @@
 // MIT License
 // See LICENSE file in the project root for full license information.
 
-#include "videnc.h"
 #include <functional>
 #include <stdexcept>
 #include <string>
+#include "logger.h"
+#include "videnc.h"
+
+extern std::unique_ptr<logger_t> logger;
 
 videnc::videnc(const config& config)
   : width(config.frame_width),
@@ -14,12 +17,16 @@ videnc::videnc(const config& config)
 
   codec = avcodec_find_encoder_by_name("libx264");
   if (!codec) {
-    throw std::runtime_error("Could not find libx264 encoder");
+    const char* err = "Could not find libx264 encoder";
+    logger->log(logger_t::level_t::ERROR, __FILE__, __LINE__, err);
+    throw std::runtime_error(err);
   }
 
   ctx = avcodec_alloc_context3(codec);
   if (!ctx) {
-    throw std::runtime_error("Could not allocate encoder context");
+    const char* err = "Could not allocate encoder context";
+    logger->log(logger_t::level_t::ERROR, __FILE__, __LINE__, err);
+    throw std::runtime_error(err);
   }
 
   ctx->width = width;
@@ -31,19 +38,23 @@ videnc::videnc(const config& config)
 
   AVDictionary *opts = NULL;
   av_dict_set(&opts, "preset", config.enc_speed.c_str(), 0);
-  av_dict_set(&opts, "crf", std::to_string(config.enc_quality.c_str(), 0);
+  av_dict_set(&opts, "crf", config.enc_quality.c_str(), 0);
 
   if (avcodec_open2(ctx, codec, &opts) < 0) {
     av_dict_free(&opts);
     avcodec_free_context(&ctx);
-    throw std::runtime_error("Could not open codec");
+    const char* err = "Could not open codec";
+    logger->log(logger_t::level_t::ERROR, __FILE__, __LINE__, err);
+    throw std::runtime_error(err);
   }
   av_dict_free(&opts);
 
   frame = av_frame_alloc();
   if (!frame) {
     avcodec_free_context(&ctx);
-    throw std::runtime_error("Could not allocate frame");
+    const char* err = "Could not allocate frame";
+    logger->log(logger_t::level_t::ERROR, __FILE__, __LINE__, err);
+    throw std::runtime_error(err);
   }
 
   frame->format = ctx->pix_fmt;
@@ -53,14 +64,18 @@ videnc::videnc(const config& config)
   if (av_frame_get_buffer(frame, 0) < 0) {
     av_frame_free(&frame);
     avcodec_free_context(&ctx);
-    throw std::runtime_error("Could not allocate frame data");
+    const char* err = "Could not allocate frame data";
+    logger->log(logger_t::level_t::ERROR, __FILE__, __LINE__, err);
+    throw std::runtime_error(err);
   }
 
   pkt = av_packet_alloc();
   if (!pkt) {
     av_frame_free(&frame);
     avcodec_free_context(&ctx);
-    throw std::runtime_error("Could not allocate packet");
+    const char* err = "Could not allocate packet";
+    logger->log(logger_t::level_t::ERROR, __FILE__, __LINE__, err);
+    throw std::runtime_error(err);
   }
 }
 
@@ -70,7 +85,11 @@ void videnc::encode_frame(uint8_t* yuv420_data, pkt_callback cb, connection& con
   const int y_size = width * height;
   const int uv_size = y_size / 4;
 
-  av_frame_make_writable(frame);
+  if (av_frame_make_writable(frame) < 0) {
+    const char* err = "Frame is not writable";
+    logger->log(logger_t::level_t::ERROR, __FILE__, __LINE__, err);
+    throw std::runtime_error(err);
+  }
 
   memcpy(frame->data[0], yuv420_data, y_size);
   memcpy(frame->data[1], yuv420_data + y_size, uv_size);
@@ -78,18 +97,27 @@ void videnc::encode_frame(uint8_t* yuv420_data, pkt_callback cb, connection& con
 
   frame->pts = pts_counter++;
 
-  if (avcodec_send_frame(ctx, frame) < 0)
-    throw std::runtime_error("Error sending frame for encoding");
+  if (avcodec_send_frame(ctx, frame) < 0) {
+    const char* err = "Error sending frame for encoding";
+    logger->log(logger_t::level_t::ERROR, __FILE__, __LINE__, err);
+    throw std::runtime_error(err);
+  }
 
   while (true) {
     int ret = avcodec_receive_packet(ctx, pkt);
     if (ret == AVERROR(EAGAIN) || ret == AVERROR_EOF)
       break;
-    if (ret < 0)
-      throw std::runtime_error("Error receiving encoded packet");
+    if (ret < 0) {
+      const char* err = "Error receiving encoded packet";
+      logger->log(logger_t::level_t::ERROR, __FILE__, __LINE__, err);
+      throw std::runtime_error(err);
+    }
 
-    if (cb(conn, pkt->data, pkt->size) < 0)
-      throw std::runtime_error("Error in stream callback");
+    if (cb(conn, pkt->data, pkt->size) < 0) {
+      const char* err = "Error in stream callback";
+      logger->log(logger_t::level_t::ERROR, __FILE__, __LINE__, err);
+      throw std::runtime_error(err);
+    }
 
     av_packet_unref(pkt);
   }
