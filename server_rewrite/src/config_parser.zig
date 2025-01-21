ff55dd41fe1b6cc4f97f3f89e1f2f6290f80c077
Alec Fessler <afessler1998@gmail.com>
2025-01-21 14:46:20 -0500

    finished writing zig config parser

--- server_rewrite/src/config_parser.zig
+++ server_rewrite/src/config_parser.zig
@@ -2,11 +2,104 @@
 //! Not a general purpose yaml parser
 
 const std = @import("std");
+const hash = std.hash.Fnv1a_64.hash;
+
+/// Copy from src to dest with 0 padding at the end of dest
+fn cpy_w_padding(dest: []u8, src: []const u8) void {
+    @memset(dest, 0);
+    @memcpy(dest[0..src.len], src);
+}
+
+const FieldAssignError = error{InvalidField} || std.fmt.ParseIntError;
+
+const StreamParams = struct {
+    const Self = @This();
+    const KeyHashes = struct {
+        const frame_width = hash("frame_width:");
+        const frame_height = hash("frame_height:");
+        const fps = hash("fps:");
+    };
+
+    frame_width: u16,
+    frame_height: u16,
+    fps: u8,
+
+    fn assign(self: *Self, key: []const u8, val: []const u8) FieldAssignError!void {
+        const hashed = hash(key);
+        switch (hashed) {
+            KeyHashes.frame_width => {
+                self.frame_width = try std.fmt.parseInt(u16, val, 10);
+            },
+            KeyHashes.frame_height => {
+                self.frame_height = try std.fmt.parseInt(u16, val, 10);
+            },
+            KeyHashes.fps => {
+                self.fps = try std.fmt.parseInt(u8, val, 10);
+            },
+            else => return error.InvalidField,
+        }
+    }
+};
+
+const CameraConfig = struct {
+    const Self = @This();
+    const KeyHashes = struct {
+        const name = hash("name:");
+        const id = hash("id:");
+        const eth_ip = hash("eth_ip:");
+        const wifi_ip = hash("wifi_ip:");
+        const tcp_port = hash("tcp_port:");
+        const udp_port = hash("udp_port:");
+    };
+
+    name: [8]u8, // name is always exactly 8 bytes
+    id: u8,
+    eth_ip_array: [16]u8, // size of ips may vary
+    eth_ip: []const u8,
+    wifi_ip_array: [16]u8,
+    wifi_ip: []const u8,
+    tcp_port: u16,
+    udp_port: u16,
+
+    fn assign(self: *Self, key: []const u8, val: []const u8) FieldAssignError!void {
+        const hashed = hash(key);
+        switch (hashed) {
+            KeyHashes.name => {
+                cpy_w_padding(&self.name, val);
+            },
+            KeyHashes.id => {
+                self.id = try std.fmt.parseInt(u8, val, 10);
+            },
+            KeyHashes.eth_ip => {
+                cpy_w_padding(&self.eth_ip_array, val);
+                self.eth_ip = self.eth_ip_array[0..val.len];
+            },
+            KeyHashes.wifi_ip => {
+                cpy_w_padding(&self.wifi_ip_array, val);
+                self.wifi_ip = self.wifi_ip_array[0..val.len];
+            },
+            KeyHashes.tcp_port => {
+                self.tcp_port = try std.fmt.parseInt(u16, val, 10);
+            },
+            KeyHashes.udp_port => {
+                self.udp_port = try std.fmt.parseInt(u16, val, 10);
+            },
+            else => {
+                return error.InvalidField;
+            },
+        }
+    }
+};
+
+const Config = struct {
+    stream_params: StreamParams,
+    camera_configs: []CameraConfig,
+};
 
 const SectionHashes = struct {
-    const stream_params = std.hash.Fnv1a_64.hash("stream_params:");
-    const cameras = std.hash.Fnv1a_64.hash("cameras:");
-    const new_camera = std.hash.Fnv1a_64.hash("- ");
+    const stream_params = hash("stream_params:");
+    const cameras = hash("cameras:");
+    const new_camera = hash("- ");
 };
 
 const NoDelimiter = error{NoDelimiter};
@@ -43,10 +136,10 @@ fn count_cameras(file_buffer: []const u8) NoDelimiter!u64 {
         offset += line.len;
 
         const trimmed = trim(line);
-        const hash = std.hash.Fnv1a_64.hash(trimmed);
+        const hashed = hash(trimmed);
 
         // look for 'cameras:' section header
-        if (hash == SectionHashes.cameras) {
+        if (hashed == SectionHashes.cameras) {
             in_cameras_list = true;
             continue;
         }
@@ -55,7 +148,7 @@ fn count_cameras(file_buffer: []const u8) NoDelimiter!u64 {
         if (trimmed.len < 2 or !in_cameras_list) continue;
 
         // check if the leading 2 chars indicate a new camera
-        const leading_hash = std.hash.Fnv1a_64.hash(trimmed[0..2]);
+        const leading_hash = hash(trimmed[0..2]);
         if (leading_hash == SectionHashes.new_camera) {
             camera_count += 1;
         }
@@ -64,16 +157,24 @@ fn count_cameras(file_buffer: []const u8) NoDelimiter!u64 {
     return camera_count;
 }
 
-const ParseError = NoDelimiter || std.fs.File.OpenError || std.fs.File.ReadError;
+const ParseError = NoDelimiter || FieldAssignError || std.mem.Allocator.Error || std.fs.File.OpenError || std.fs.File.ReadError;
 
 /// Parses the camera config yaml file
-pub fn parse(filepath: []const u8) ParseError!void {
+pub fn parse(allocator: std.mem.Allocator, filepath: []const u8) ParseError!Config {
     const file = try std.fs.openFileAbsolute(filepath, .{ .mode = .read_only });
+    defer file.close();
+
     var file_buffer: [1024]u8 = undefined; // may need to increase if cam_count grows larger, for now, at 3 cameras the file is ~500 bytes
     const bytes_read = try file.readAll(&file_buffer);
 
     const camera_count = try count_cameras(file_buffer[0..bytes_read]);
-    std.debug.print("Camera count {}\n", .{camera_count});
+    var cameras_parsed: u64 = 0;
+
+    var config = Config{
+        .stream_params = undefined,
+        .camera_configs = try allocator.alloc(CameraConfig, camera_count),
+    };
+    errdefer allocator.free(config.camera_configs);
 
     const ConfigSection = enum { stream_params, cameras, none };
     var section: ConfigSection = .none;
@@ -86,8 +187,8 @@ pub fn parse(filepath: []const u8) ParseError!void {
 
         switch (section) {
             .none => {
-                const hash = std.hash.Fnv1a_64.hash(trimmed);
-                section = switch (hash) {
+                const hashed = hash(trimmed);
+                section = switch (hashed) {
                     SectionHashes.stream_params => .stream_params,
                     SectionHashes.cameras => .cameras,
                     else => .none,
@@ -100,22 +201,25 @@ pub fn parse(filepath: []const u8) ParseError!void {
                 }
 
                 const key = try read_until(trimmed, ':');
-                const val = trimmed[key.len..];
-                std.debug.print("Key {s}: Val {s}\n", .{ key, val });
+                const val = trim(trimmed[key.len..]);
+                try config.stream_params.assign(key, val);
             },
             .cameras => {
                 if (trimmed.len == 0) { // end of single camera config
+                    cameras_parsed += 1;
                     continue;
                 }
 
                 var key = try read_until(trimmed, ':');
-                const val = trimmed[key.len..];
+                const val = trim(trimmed[key.len..]);
 
-                const leading_hash = std.hash.Fnv1a_64.hash(key[0..2]);
+                const leading_hash = hash(key[0..2]);
                 if (leading_hash == SectionHashes.new_camera) key = key[2..];
 
-                std.debug.print("Key {s}: Val {s}\n", .{ key, val });
+                try config.camera_configs[cameras_parsed].assign(key, val);
             },
         }
     }
+
+    return config;
 }
--- server_rewrite/src/main.zig
+++ server_rewrite/src/main.zig
@@ -9,7 +9,24 @@ pub fn main() !void {
     try log.setup(LOG_PATH);
     defer log.cleanup();
 
-    try config_parser.parse(CAM_CONFIG_PATH);
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const config = try config_parser.parse(allocator, CAM_CONFIG_PATH);
+
+    std.debug.print("frame_width: {}\nframe_height: {}\nfps: {}\n\n", .{ config.stream_params.frame_width, config.stream_params.frame_height, config.stream_params.fps });
+
+    for (config.camera_configs) |camera_config| {
+        std.debug.print("name: {s}\nid: {}\neth_ip: {s}\nwifi_ip: {s}\ntcp_port: {}\nudp_port: {}\n\n", .{
+            camera_config.name,
+            camera_config.id,
+            camera_config.eth_ip,
+            camera_config.wifi_ip,
+            camera_config.tcp_port,
+            camera_config.udp_port,
+        });
+    }
 
     try log.write(.INFO, "Hello world\n", @src());
 }
