529abcb021a91cd02f9f8d250eddb574991583d0
Alec Fessler <afessler1998@gmail.com>
2025-01-24 14:58:50 -0500

    moved shared memory size and layout initialization to comptime

--- server_rewrite/src/main.zig
+++ server_rewrite/src/main.zig
@@ -7,50 +7,42 @@ const sched = @import("sched.zig");
 const shared_mem_builder = @import("shared_mem_builder.zig");
 const spscq = @import("spsc_queue.zig");
 
-const LOG_PATH: []const u8 = "/var/log/mocap-toolkit/server.log";
-
-// null terminated string for compatibility with linux c shm_open
-const SHM_NAME: [*:0]const u8 = "/mocap-toolkit_shm";
-// many-item ptr for matching type of std.posix.mmap's first arg
-const SHM_ADDR: [*]align(std.mem.page_size) u8 = @ptrFromInt(0x7f0000000000);
-
-// The config is built directly into the executable
-// this is done because it means the entire memory layout
-// is all known at comptime which cuts down on startup time
-// and allows the compiler to make optimizations it otherwise can't
 const config_bytes = @embedFile("cams.yaml");
 const ConfigType = config_parser.Config(config_bytes);
 const config = config_parser.parse(ConfigType, config_bytes);
 
 const yuv_size: u64 = config.stream_params.frame_width * config.stream_params.frame_height * 3 / 2;
-
 const Frame = frame.Frame(yuv_size);
-const RingAllocator = ring_alloc.RingAllocator(Frame);
 const SPSCQueue = spscq.Queue(*Frame);
-const SharedMemBuilder = shared_mem_builder.SharedMemBuilder;
+const RingAllocator = ring_alloc.RingAllocator(Frame);
+const SharedMemBuilder = shared_mem_builder.SharedMemBuilder();
+
+const num_frames = 12;
+const num_queue_slots = num_frames - 2;
+
+const frames_reservation = SharedMemBuilder.reserve(Frame, num_frames, 0);
+const queue_slots_reservation = SharedMemBuilder.reserve(*Frame, num_queue_slots, frames_reservation.shm_size);
+const queue_reservation = SharedMemBuilder.reserve(SPSCQueue, 1, queue_slots_reservation.shm_size);
+
+const shm_size = queue_reservation.shm_size;
+const shm_name: [*:0]const u8 = "/mocap-toolkit_shm";
+const shm_addr: [*]align(std.mem.page_size) u8 = @ptrFromInt(0x7f0000000000);
+
+const log_path: []const u8 = "/var/log/mocap-toolkit/server.log";
 
 pub fn main() !void {
-    try log.setup(LOG_PATH);
+    try log.setup(log_path);
     defer log.cleanup();
 
     try sched.pin_to_core(0);
     try sched.set_sched_priority(99);
 
-    var shm_builder = SharedMemBuilder.init();
-    defer shm_builder.deinit();
+    var shared_mem = try SharedMemBuilder.init(shm_name, shm_addr, shm_size);
+    defer shared_mem.deinit();
 
-    const num_frames: u64 = 12;
-    const num_queue_slots: u64 = num_frames - 2;
-
-    const frames_offset = shm_builder.reserve(Frame, num_frames);
-    const queue_slots_offset = shm_builder.reserve(*Frame, num_queue_slots);
-    const queue_offset = shm_builder.reserve(SPSCQueue, 1);
-
-    try shm_builder.allocate(SHM_NAME, SHM_ADDR);
-
-    const frames_slice = shm_builder.get_slice(Frame, frames_offset, num_frames);
-    const queue_slots = shm_builder.get_slice(*Frame, queue_slots_offset, num_queue_slots);
-    var queue = shm_builder.get_slice(SPSCQueue, queue_offset, 1)[0];
+    const frames_slice = shared_mem.get_slice(Frame, frames_reservation.offset, num_frames);
+    const queue_slots = shared_mem.get_slice(*Frame, queue_slots_reservation.offset, num_queue_slots);
+    var queue = shared_mem.get_item(SPSCQueue, queue_reservation.offset);
 
     queue.init(queue_slots);
     var ring_allocator = RingAllocator.init(frames_slice);
--- server_rewrite/src/shared_mem_builder.zig
+++ server_rewrite/src/shared_mem_builder.zig
@@ -1,70 +1,78 @@
 const std = @import("std");
 
-pub const SharedMemBuilder = struct {
-    const Self = @This();
+pub fn SharedMemBuilder() type {
+    return struct {
+        const Self = @This();
 
-    fd: i32,
-    buffer: []align(std.mem.page_size) u8,
-    size: u64,
+        buffer: []align(std.mem.page_size) u8,
 
-    pub fn init() Self {
-        return .{
-            .fd = -1,
-            .buffer = undefined,
-            .size = 0,
+        const Reservation = struct {
+            offset: u64,
+            shm_size: u64,
         };
-    }
 
-    pub fn deinit(self: *Self) void {
-        std.posix.munmap(self.buffer);
-    }
+        pub fn reserve(comptime T: type, comptime count: u64, comptime current_size: u64) Reservation {
+            const offset = align_up(current_size, @alignOf(T));
+            const new_size = current_size + offset + @sizeOf(T) * count;
+            return Reservation{
+                .offset = offset,
+                .shm_size = new_size,
+            };
+        }
 
-    fn align_up(offset: u64, alignment: u64) u64 {
-        return (offset + (alignment - 1)) & ~(alignment - 1);
-    }
+        fn align_up(comptime offset: u64, comptime alignment: u64) u64 {
+            return (offset + (alignment - 1)) & ~(alignment - 1);
+        }
 
-    pub fn reserve(self: *Self, T: type, count: u64) u64 {
-        const offset = align_up(self.size, @alignOf(T));
-        self.size += offset + @sizeOf(T) * count;
-        return offset;
-    }
-
-    pub fn allocate(self: *Self, shm_name: [*:0]const u8, shm_addr: ?[*]align(std.mem.page_size) u8) !void {
-        const flags: std.posix.O = .{
-            .ACCMODE = .RDWR,
-            .CREAT = true,
-        };
-        const permissions: std.posix.mode_t = 0o666;
-        self.fd = std.c.shm_open(shm_name, @bitCast(flags), permissions);
-        if (self.fd == -1) {
-            const errnum: u32 = @bitCast(std.c._errno().*);
-            const err: std.posix.E = @enumFromInt(errnum);
-            switch (err) {
-                .SUCCESS => unreachable,
-                .ACCES => return error.AccessDenied,
-                .EXIST => return error.PathAlreadyExists,
-                .INVAL => return error.InvalidInput,
-                .MFILE => return error.ProcessFdQuotaExceeded,
-                .NAMETOOLONG => return error.NameTooLong,
-                .NFILE => return error.SystemFdQuotaExceeded,
-                .NOENT => return error.FileNotFound,
-                else => return std.posix.unexpectedErrno(err),
+        pub fn init(shm_name: [*:0]const u8, shm_addr: ?[*]align(std.mem.page_size) u8, size: u64) !Self {
+            const flags: std.posix.O = .{
+                .ACCMODE = .RDWR,
+                .CREAT = true,
+            };
+            const permissions: std.posix.mode_t = 0o666;
+            const fd = std.c.shm_open(shm_name, @bitCast(flags), permissions);
+            if (fd == -1) {
+                const errnum: u32 = @bitCast(std.c._errno().*);
+                const err: std.posix.E = @enumFromInt(errnum);
+                switch (err) {
+                    .SUCCESS => unreachable,
+                    .ACCES => return error.AccessDenied,
+                    .EXIST => return error.PathAlreadyExists,
+                    .INVAL => return error.InvalidInput,
+                    .MFILE => return error.ProcessFdQuotaExceeded,
+                    .NAMETOOLONG => return error.NameTooLong,
+                    .NFILE => return error.SystemFdQuotaExceeded,
+                    .NOENT => return error.FileNotFound,
+                    else => return std.posix.unexpectedErrno(err),
+                }
             }
+            defer std.posix.close(fd);
+
+            try std.posix.ftruncate(fd, size);
+
+            const prot_flags: u32 = std.posix.PROT.READ | std.posix.PROT.WRITE;
+            const buffer = try std.posix.mmap(shm_addr, @intCast(size), prot_flags, .{ .TYPE = .SHARED }, fd, 0);
+
+            if (shm_addr) |addr| {
+                try std.testing.expect(buffer.ptr == addr);
+            }
+
+            return .{
+                .buffer = buffer,
+            };
         }
-        defer std.posix.close(self.fd);
 
-        try std.posix.ftruncate(self.fd, self.size);
-
-        const prot_flags: u32 = std.posix.PROT.READ | std.posix.PROT.WRITE;
-        self.buffer = try std.posix.mmap(shm_addr, @intCast(self.size), prot_flags, .{ .TYPE = .SHARED }, self.fd, 0);
-
-        if (shm_addr) |addr| {
-            try std.testing.expect(self.buffer.ptr == addr);
+        pub fn deinit(self: *Self) void {
+            std.posix.munmap(self.buffer);
         }
-    }
 
-    pub fn get_slice(self: *Self, T: type, offset: u64, count: u64) []T {
-        const typed_ptr: [*]T = @alignCast(@ptrCast(&self.buffer[offset]));
-        return typed_ptr[0..count];
-    }
-};
+        pub fn get_slice(self: *const Self, T: type, offset: u64, count: u64) []T {
+            const typed_ptr: [*]T = @alignCast(@ptrCast(&self.buffer[offset]));
+            return typed_ptr[0..count];
+        }
+
+        pub fn get_item(self: *const Self, T: type, offset: u64) *T {
+            return @alignCast(@ptrCast(&self.buffer[offset]));
+        }
+    };
+}
